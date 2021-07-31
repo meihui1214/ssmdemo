c9d5a7dd261bf55b2826dd77eabfa06090131648
张美惠 <zhangmeihui1214@outlook.com>
2021-07-31 19:27:58 +0800

    测试MAP回数据

--- src/main/java/com/example/ssmdemo/mapper/UserMapper.java
+++ src/main/java/com/example/ssmdemo/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    List<User> select(User user);
+    List<User> select();
     int insert(User user);
     User SelectByUIdAndPassword(@Param("uId")Integer uId,@Param("password") String password);
     User selectUIdByEmail(@Param("email") String email);
--- src/main/java/com/example/ssmdemo/service/LoginService.java
+++ src/main/java/com/example/ssmdemo/service/LoginService.java
@@ -2,7 +2,10 @@ package com.example.ssmdemo.service;
 
 import com.example.ssmdemo.domain.User;
 
+import java.util.List;
+
 public interface LoginService {
     public User login(Integer uId, String password);
     public User selectUIdByEmail(String email);
+    public List<User> selectAll();
 }
