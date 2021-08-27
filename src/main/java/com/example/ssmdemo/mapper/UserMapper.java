e7cda309260b52d0f5e0e76db77ed8c0b2bc726d
张美惠 <zhangmeihui1214@outlook.com>
2021-08-27 13:42:34 +0800

    测试动态SQL 以及foreach 标签list 和 马屁

--- src/main/java/com/example/ssmdemo/mapper/UserMapper.java
+++ src/main/java/com/example/ssmdemo/mapper/UserMapper.java
@@ -5,12 +5,21 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface UserMapper {
+    List<User> selectByEmailAndPassword(@Param("emails") Map<String, String> emails, @Param("params") Map<String, String> params);
+
+    List<User> selectAllByEmailOrPasswordAndPassword(User user);
+
     List<User> select();
+
     int insert(User user);
-    User SelectByEmailAndPassword(@Param("email")String email,@Param("password") String password);
+
+    User SelectByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
     User selectUIdByEmail(@Param("email") String email);
+
     int deleteByUId(@Param("uId") Integer uId);
 }
--- src/test/java/com/example/ssmdemo/SsmdemoApplicationTests.java
+++ src/test/java/com/example/ssmdemo/SsmdemoApplicationTests.java
@@ -5,10 +5,16 @@ import com.example.ssmdemo.mapper.UserMapper;
 import com.example.ssmdemo.service.LoginService;
 import com.example.ssmdemo.service.impl.LoginserviceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SsmDemoApplicationTests {
@@ -19,6 +25,31 @@ class SsmDemoApplicationTests {
 
     @Test
     void contextzdLoads() {
-        userMapper.SelectByEmailAndPassword("zhangmeihui","123456789");
+        userMapper.SelectByEmailAndPassword("zhangmeihui", "123456789");
+    }
+
+    @Test
+    void dynamicSql() {
+        User user = new User();
+        userMapper.selectAllByEmailOrPasswordAndPassword(user).forEach(System.out::println);
+    }
+
+    @Test
+    void foreachMapSql() {
+        Map<String, String> stringPassMap = new HashMap<>();
+        stringPassMap.put("password1", "2");
+        stringPassMap.put("password8", "123456789");
+        stringPassMap.put("password2", "123456789");
+        stringPassMap.put("password3", "123456789");
+        stringPassMap.put("password4", "123456789");
+        stringPassMap.put("password5", "123456789");
+        stringPassMap.put("password6", "123456789");
+        stringPassMap.put("password7", "123456789");
+
+        Map<String, String> stringEmailMap = new HashMap<>();
+        stringEmailMap.put("email","zhangmeihui");
+        stringEmailMap.put("email1","yughjkbh");
+
+        userMapper.selectByEmailAndPassword(stringEmailMap, stringPassMap).forEach(System.out::println);
     }
 }
