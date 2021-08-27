60008572506d82b5536204d28c0d09f0cf14c393
张美惠 <zhangmeihui1214@outlook.com>
2021-08-27 15:27:49 +0800

    联系关联映射的左连接

--- src/main/java/com/example/ssmdemo/mapper/LogMapper.java
+++ src/main/java/com/example/ssmdemo/mapper/LogMapper.java
@@ -2,10 +2,12 @@ package com.example.ssmdemo.mapper;
 
 import com.example.ssmdemo.domain.Log;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface LogMapper {
-    List<Log> selectAllLogs();
+    List<Log> selectAllLogs(@Param("logId")Integer logId);
+    int insert (Log log);
 }
--- src/test/java/com/example/ssmdemo/SsmdemoApplicationTests.java
+++ src/test/java/com/example/ssmdemo/SsmdemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.ssmdemo;
 
+import com.example.ssmdemo.domain.Log;
 import com.example.ssmdemo.domain.User;
+import com.example.ssmdemo.mapper.LogMapper;
 import com.example.ssmdemo.mapper.UserMapper;
 import com.example.ssmdemo.service.LoginService;
 import com.example.ssmdemo.service.impl.LoginserviceImpl;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.w3c.dom.ls.LSOutput;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,7 +24,10 @@ class SsmDemoApplicationTests {
     @Autowired
     private LoginService loginService;
     @Autowired
-    UserMapper userMapper;
+   private UserMapper userMapper;
+
+    @Autowired
+   private LogMapper logMapper;
 
     @Test
     void contextzdLoads() {
@@ -39,17 +45,27 @@ class SsmDemoApplicationTests {
         Map<String, String> stringPassMap = new HashMap<>();
         stringPassMap.put("password1", "2");
         stringPassMap.put("password8", "123456789");
-        stringPassMap.put("password2", "123456789");
-        stringPassMap.put("password3", "123456789");
-        stringPassMap.put("password4", "123456789");
-        stringPassMap.put("password5", "123456789");
-        stringPassMap.put("password6", "123456789");
-        stringPassMap.put("password7", "123456789");
 
         Map<String, String> stringEmailMap = new HashMap<>();
         stringEmailMap.put("email","zhangmeihui");
         stringEmailMap.put("email1","yughjkbh");
 
         userMapper.selectByEmailAndPassword(stringEmailMap, stringPassMap).forEach(System.out::println);
+    }
+
+    @Test
+    void logLeftUserSql(){
+        logMapper.selectAllLogs(1).forEach(System.out::println);
+    }
+    @Test
+    void insertLog(){
+        Log log =new Log();
+        log.setLogId(1);
+        User user = new User();
+        user.setuId(1);
+        log.setUser(user);
+        log.setLoginCount(1);
+        log.setLastLoginTime(LocalDateTime.now());
+        logMapper.insert(log);
     }
 }
